e77fe7892c747efb6b2ac53172e697b36b96cd9e
tttriplicate <tttriplicate@gmail.com>
2023-07-18 17:00:42 -0400

    a decent state

--- translator.py
+++ translator.py
@@ -12,6 +12,44 @@ for i, line in enumerate(intake):
         else:
             results[i] += '0'
 intake.close()
-with open('result_correct_offset_binary.txt', 'w') as output:
-    output.write('\n'.join(results))
-output.close()
+
+interpreter = {
+    '01000': '\n',
+    '00010': '',
+    '10111': 'q',
+    '10011': 'w',
+    '00001': 'e',
+    '01010': 'r',
+    '10000': 't',
+    '10101': 'y',
+    '00111': 'u',
+    '00110': 'i',
+    '11000': 'o',
+    '10110': 'p',
+    '00011': 'a',
+    '00101': 's',
+    '01001': 'd',
+    '01101': 'f',
+    '11010': 'g',
+    '10100': 'h',
+    '01011': 'j',
+    '01111': 'k',
+    '10010': 'l',
+    '10001': 'z',
+    '11101': 'x',
+    '01110': 'c',
+    '11110': 'v',
+    '11001': 'b',
+    '01100': 'n',
+    '11100': 'm',
+    '11111': '*',
+    '00000': ' ',
+    '00100': ' ',
+    '11011': '^'
+}
+
+with open('current.txt','w') as current:
+    current.write(''.join(results))
+with open('read.txt', 'w') as writer:
+    writer.write(''.join([interpreter[i] for i in results]))
+
